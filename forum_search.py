b4fd9a7f20af734160957f3dc9ff0cd27d282365
LadyBug0805 <117118340+LadyBug0805@users.noreply.github.com>
2023-01-26 18:35:19 +0100

    Added a maximize window

--- forum_search.py
+++ forum_search.py
@@ -7,9 +7,10 @@ PATH = "C:\Program Files\Selenium\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://70000tons.com/forum/")
+driver.maximize_window()
 print(driver.title)
 
-search =  driver.find_element(By.CLASS_NAME, "form-control")
+search = driver.find_element(By.CLASS_NAME, "form-control")
 search.send_keys("Slayer")
 search.send_keys(Keys.RETURN)
 time.sleep(5)
