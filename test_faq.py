fce8a8f1b0594738c857c5ab90cac3fe84e43520
LadyBug0805 <117118340+LadyBug0805@users.noreply.github.com>
2023-02-13 18:03:02 +0100

    Removed time.sleep and added Explicit wait instead

--- test_faq.py
+++ test_faq.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 #from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 #from selenium.webdriver.support.select import Select
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 PATH = "C:\Program Files\Selenium\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -14,5 +15,6 @@ select_language.click()
 
 faq = driver.find_element(By.ID, "umc-div-menu-faq")
 faq.click()
-time.sleep(3)
+faq =WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.XPATH, "/html/body/div[1]/header/nav/div/ul/li[6]/a/div"))
+#simple_assert(faq.text, "FREQUENTLY ASKED QUESTIONS") 
 driver.close()
