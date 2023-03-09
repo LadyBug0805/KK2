ca3fe850f8063c07c70e03a440cfd369e83bdf9f
Angelica Tofte <bromolla_tjej@hotmail.com>
2023-03-09 15:39:47 +0100

    Rename file of test and adding quit

---
+++ test_forum_search.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+PATH = "C:\Program Files\Selenium\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://70000tons.com/forum/")
+driver.maximize_window()
+print(driver.title)
+
+search = driver.find_element(By.CLASS_NAME, "form-control")
+search.clear()
+search.send_keys("Slayer")
+search.send_keys(Keys.RETURN)
+time.sleep(5)
+driver.quit()
+
