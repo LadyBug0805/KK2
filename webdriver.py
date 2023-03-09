e06f9703598ef0acaaf34a1f3414e6543c4c89d7
Angelica Tofte <bromolla_tjej@hotmail.com>
2023-03-09 20:07:37 +0100

    Remove headless option

---
+++ webdriver.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+def get_webdriver():
+    options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    options.add_argument('--disable-infobars')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--remote-debugging-port=9222')
+    return webdriver.Chrome(options=options)
