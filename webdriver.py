0de5af0929aca6043891582e4179cdda69ef6e66
Angelica Tofte <bromolla_tjej@hotmail.com>
2023-03-14 17:09:56 +0100

    Update webdriver.py

--- webdriver.py
+++ webdriver.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 def get_webdriver():
     options = webdriver.ChromeOptions()
-    #options.add_argument('headless')
+    options.add_argument('headless')
     options.add_argument('--disable-infobars')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
